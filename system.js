6e41dce9bab0739fc90471bc7952b9a03c466c7a
bonnerowl <118012146+bonnerowl@users.noreply.github.com>
2022-11-12 17:42:32 -0300

    Update system.js

--- system.js
+++ system.js
@@ -1,1 +1,5 @@
 console.log("test working");
+
+let slide = querySelector('.e-widget-swiper');
+
+slide.setAttribute("data-settings", "{&quot;autoplay_speed&quot;:500,&quot;navigation&quot;:&quot;both&quot;,&quot;autoplay&quot;:&quot;yes&quot;,&quot;pause_on_hover&quot;:&quot;yes&quot;,&quot;pause_on_interaction&quot;:&quot;yes&quot;,&quot;infinite&quot;:&quot;yes&quot;,&quot;transition&quot;:&quot;slide&quot;,&quot;transition_speed&quot;:500}");
